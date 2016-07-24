f8ffe39ba87caa1234482573acdd4c4ddc9a84d8
Petr Semiletov <tea@list.ru>
2016-07-24 22:42:13 +0000

    upd

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # mtag
+MTAG
+=============================
+
 mtag is the command-line tool for media files tagging
+
+
+REQUIREMENTS
+------------
+
+Taglib, C++ 11
+
+INSTALLATION
+------------
+
+make
+
+
+USE
+------------
+
+
+OTHER
+------------
--- main.cpp
+++ main.cpp
@@ -11,17 +11,6 @@
 #include <fileref.h>
 #include <tstring.h>
 
-/*
-#include <mpegfile.h>
-
-
-
-#include <id3v2tag.h>
-#include <id3v2frame.h>
-#include <id3v2header.h>
-
-#include <id3v1tag.h>
-*/
 
 using namespace std;
 using namespace TagLib;
@@ -29,12 +18,12 @@ using namespace TagLib;
 int main (int argc, char *argv[])
 {
   if (argc < 3) 
-  {
-   cout << "mtag RULESFILE filemask" <<  endl;
-   return 0;
-  }
+     {
+      cout << "mtag RULESFILE filemask" <<  endl;
+      return 0;
+     }
  
-   cout << "START" <<  endl;
+  cout << "START" <<  endl;
  
  
   string rules_file = argv[1];
@@ -45,65 +34,16 @@ int main (int argc, char *argv[])
   
   std::sort (files.begin(), files.end());
   
-  /*
-  for (const auto &t: files)
-      {  
-       cout << t << endl;
-     
-      }
-  */
   
   string rules_file_data = string_file_load (rules_file);
  
  // rules_file_data = rules_file_data.insert (0, ";\n");
    
-  //cout << rules_file_data << endl;
   
   vector<string> vs = string_split (rules_file_data, "###");
    
-  cout << "vs.size: " << vs.size() << endl;
+ // cout << "vs.size: " << vs.size() << endl;
   
-  
- /* 
-  for (auto &element: vs)
-      {
-      // if (element.empty())
-        //  continue;
-       
-       
-       CPairFile pf (element, true);
-       cout << pf.get_string ("@artist", "1") << endl;
-       cout << pf.get_string ("@title", "1") << endl;
-       
-       //cout << pf.values.size() << endl;
-       
-      }
- */
- 
-   
- //cout << vs[0] << endl;
- 
-/* 
-   CPairFile pft (vs[0], true);
-   
-   cout << "pft.values.size is " << pft.values.size() << endl;
-   
-  std::vector<string> itemKeys;
-for (auto & kvp : pft.values)
-{
-    itemKeys.emplace_back(kvp.first);
-    //std::cout << kvp.first << std::endl;
-
-   cout << "!!!!!! " << pft.values[kvp.first] << endl;
-    
-    
-}
-  */ 
-   
- //  cout << "!!!!!! " << pft.values["@artist"] << endl;
-//   cout << "!!!!!! " << pft.values["@title"] << endl;
-   //cout << "!!!!!! " << pft.values["@album"] << endl;
-         
  
 
   for (size_t i = 0; i < vs.size(); i++)
@@ -118,66 +58,56 @@ for (auto & kvp : pft.values)
   
       for (auto & kvp : pf.values)
           {
-           //itemKeys.emplace_back(kvp.first);
-           //std::cout << kvp.first << std::endl;
 
-           cout << kvp.first << " is " << pf.values[kvp.first] << endl;
+  //         cout << kvp.first << " is " << pf.values[kvp.first] << endl;
+           
+           string key = kvp.first;
+           string val = pf.values[kvp.first];
+           
+           cout << key << " --------------------- " << val << endl;
      
-           if (kvp.first == "@artist")
+           if (key == "@artist")
               {
-               TagLib::String ts (pf.values[kvp.first], TagLib::String::Type::UTF8);
+               TagLib::String ts (val, TagLib::String::Type::UTF8);
                f.tag()->setArtist (ts);
               }  
-           
-           if (kvp.first == "@title")
+           else            
+           if (key == "@title")
               {
-               TagLib::String ts (pf.values[kvp.first], TagLib::String::Type::UTF8);
+               TagLib::String ts (val, TagLib::String::Type::UTF8);
                f.tag()->setTitle (ts);
               }  
-   
-           if (kvp.first == "@album")
+           else            
+           if (key == "@album")
               {
-               TagLib::String ts (pf.values[kvp.first], TagLib::String::Type::UTF8);
+               TagLib::String ts (val, TagLib::String::Type::UTF8);
                f.tag()->setAlbum (ts);
               }  
-           
+           else            
+           if (key == "@genre")
+              {
+               TagLib::String ts (val, TagLib::String::Type::UTF8);
+               f.tag()->setGenre (ts);
+              }  
+           else            
+           if (key == "@comment")
+              {
+               TagLib::String ts (val, TagLib::String::Type::UTF8);
+               f.tag()->setComment (ts);
+              }  
+           else            
+           if (key == "@year")
+               f.tag()->setYear (atoi(val.c_str()));
+           else            
+           if (key == "@track")
+               f.tag()->setTrack (atoi(val.c_str()));
+   
           }
 
          f.save();
    
          cout << "tag saved" << endl;
-   
       
-      
-   //   cout << "pf.values.size: "  << pf.values.size() << endl;
-      
-     /*for (size_t j = 0; j < pf.values.size(); j++)
-        {
-         qDebug() << pf.values[j] << endl;   
-      
-        }
-*/
-      
-  //     cout << "!!!!!! " << pf.values["@artist"] << endl;
-       
-       
-    //   cout << pf.get_string ("@artist", "1") << endl;
-  //     cout << pf.get_string ("@title", "1") << endl;
-      
-//      for (size_t j = 0; j < pf.values.size(); j++)
-
-      /*
-        
-       TagLib::FileRef f (fname.c_str());
-     
-       TagLib::String ts (pf.get_string ("@artist", "1"), TagLib::String::Type::UTF8);
-       f.tag()->setArtist (ts);
-       f.tag()->setTitle (pf.get_string ("@title", "1"));
-
-       f.save();
-   
-       cout << "tag saved" << endl;
-      */
      }
   
   
