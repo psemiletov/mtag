90bd2f6ff756c33acdbe89d3ed55ef4fec4e798d
Petr Semiletov <tea@list.ru>
2016-07-25 14:13:39 +0000

    fixes

--- main.cpp
+++ main.cpp
@@ -54,10 +54,17 @@ int main (int argc, char *argv[])
    parsed properly, so we programmatically add the additional line: 
    */
   
-  rules_file_data = rules_file_data.insert (3, "@test=test\n"); 
+  
+ // rules_file_data = rules_file_data.insert (3, "@testkey=testvalue\n"); 
    
   
   vector<string> vs = string_split (rules_file_data, "###");
+  
+  //vs[0] = vs[0].insert (3, "@testkey=testvalue\n"); 
+ // vs[0] = vs[0].insert (3, ";a=a\u000A"); 
+ //  vs[0] = vs[0].insert (3, ";080808808909\u000A\n"); 
+ 
+  
   
   for (size_t i = 0; i < vs.size(); i++)
      {
@@ -68,7 +75,18 @@ int main (int argc, char *argv[])
       TagLib::FileRef f (fname.c_str());
       
       CPairFile pf (vs[i], true);
-  
+
+  /*
+   Nasty hack - the first tag at the first tags block, by the mystical reason, cannot be
+   parsed properly.
+   There are two medicines from this STRANGE behaviour.
+   1. Manually (but not programmatically from mtag) 
+      add the obsolete first line to the RULES file.
+   2. Add the obsolete key-val to the tags structure.    
+   */
+
+      pf.values["@testkey"] = "testvalue";
+      
       for (auto & kvp : pf.values)
           {
            string key = kvp.first;
