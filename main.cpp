3790e9c16406ec5f6335e117197367f0b3849694
Petr Semiletov <tea@list.ru>
2016-08-01 21:04:37 +0000

    extract mode added

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
  Peter Semiletov <tea@list.ru>, http://semiletov.org
 */
 
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -23,6 +24,193 @@
 using namespace std;
 using namespace TagLib;
 
+
+void write_tags (const string &rules_file, const string &ext)
+{
+  std::vector <string> files = files_get_list (current_path(), ext);
+  std::sort (files.begin(), files.end());
+  if (files.size() == 0)
+    {
+     cout << "No files to process! Please provide some files. Read README for the details." << endl;
+     return;
+    }
+
+  string rules_file_data = string_file_load (rules_file);
+  
+  vector<string> vs = string_split (rules_file_data, "###");
+
+  for (size_t i = 0; i < vs.size(); i++)
+     {
+      string fname = files[i];
+      cout << "process:" << fname << endl;
+      TagLib::FileRef f (fname.c_str());
+      CPairFile pf (vs[i], true);
+
+      /*
+       Nasty hack - the first tag at the first tags block,
+       by the mystical reason, cannot be parsed properly.
+      There are two medicines from this STRANGE behaviour.
+      1. Manually (but not programmatically from mtag)
+      add the obsolete first line to the RULES file.
+      2. Add the obsolete key-val to the tags structure.
+      */
+
+     pf.values["@testkey"] = "testvalue";
+     for (auto & kvp : pf.values)
+        {
+         string key = kvp.first;
+         string val = pf.values[kvp.first];
+         
+         cout << key << " --------------------- " << val << endl;
+         
+         TagLib::String ts (val, TagLib::String::Type::UTF8);
+         
+         if (key == "@artist")
+            f.tag()->setArtist (ts);
+         else
+         if (key == "@title")
+            f.tag()->setTitle (ts);
+         else
+         if (key == "@album")
+            f.tag()->setAlbum (ts);
+         else
+         if (key == "@genre")
+            f.tag()->setGenre (ts);
+         else
+             if (key == "@comment")
+         f.tag()->setComment (ts);
+         else
+         if (key == "@year")
+            f.tag()->setYear (stoi (val));
+         else
+         if (key == "@track")
+            f.tag()->setTrack (stoi (val));
+        }
+
+     f.save();
+     cout << "tag: saved" << endl;
+    }
+}
+
+
+void extract_tags (const string &rules_file, const string &ext)
+{
+  std::vector <string> files = files_get_list (current_path(), ext);
+  std::sort (files.begin(), files.end());
+
+
+  if (files.size() == 0)
+    {
+     cout << "No files to process! Please provide some files. Read README for the details." << endl;
+     return;
+    }
+     
+  
+  vector<string> vs;
+  
+  for (size_t i = 0; i < files.size(); i++)
+     {
+      string fname = files[i];
+          
+      cout << "process:" << fname << endl;
+      
+      TagLib::FileRef f (fname.c_str());
+      
+      TagLib::String ts;
+      
+      ts = f.tag()->artist();
+      if (! ts.isEmpty())
+        {
+         string pair = "@artist";
+         pair += "=";
+         pair += ts.toCString(true);
+         vs.push_back (pair);
+        } 
+   
+      ts = f.tag()->title();
+      if (! ts.isEmpty())
+        {
+         string pair = "@title";
+         pair += "=";
+         pair += ts.toCString(true);
+         vs.push_back (pair);
+        } 
+   
+      ts = f.tag()->album();
+      if (! ts.isEmpty())
+        {
+         string pair = "album";
+         pair += "=";
+         pair += ts.toCString(true);
+         vs.push_back (pair);
+        } 
+   
+      ts = f.tag()->comment();
+      if (! ts.isEmpty())
+        {
+         string pair = "comment";
+         pair += "=";
+         pair += ts.toCString(true);
+         vs.push_back (pair);
+        } 
+   
+   
+      ts = f.tag()->genre();
+      if (! ts.isEmpty())
+        {
+         string pair = "genre";
+         pair += "=";
+         pair += ts.toCString(true);
+         vs.push_back (pair);
+        } 
+   
+      unsigned int x = 0;
+   
+      
+      x = f.tag()->year();
+      if (x != 0)
+        {
+         string pair = "year";
+         pair += "=";
+         pair += std::to_string (x);
+         vs.push_back (pair);
+        } 
+   
+       cout << "x:" << x << endl;
+      
+   
+      x = f.tag()->track();
+      if (x != 0)
+        {
+         string pair = "track";
+         pair += "=";
+         pair += std::to_string (x);
+         vs.push_back (pair);
+        } 
+   
+   
+      vs.push_back ("###");
+      
+      cout << "tags: extracted" << endl;
+     }
+     
+  vs.pop_back();   
+     
+  ofstream outputFile (rules_file);
+
+  if (outputFile.is_open())
+    {
+     for (string s: vs) 
+        { 
+         outputFile << s << endl; 
+        }
+    }   
+  else 
+      cout << "Unable to write output file" << endl;
+
+     
+}
+
 int main (int argc, char *argv[])
 {
   if (argc < 3) 
@@ -37,98 +225,22 @@ int main (int argc, char *argv[])
   string ext = argv[2];
   ext = "." + ext;
   
-  std::vector <string> files = files_get_list (current_path(), ext);
-  std::sort (files.begin(), files.end());
-
-
-  if (files.size() == 0)
+  if (argc < 4)
     {
-     cout << "No files to process! Please provide some files. Read README for the details." << endl;
-     return 0;
+     write_tags (rules_file, ext);
+     return 0;   
     }
   
-  string rules_file_data = string_file_load (rules_file);
- 
-  /*
-   Nasty hack - the first line at the tags block, by the mystical reason, cannot be
-   parsed properly, so we programmatically add the additional line: 
-   */
+  //
   
-  
- // rules_file_data = rules_file_data.insert (3, "@testkey=testvalue\n"); 
+  string mode = argv[3];
+  if (mode == "extract")  
+    {
+     cout << "extract" << endl;
+     extract_tags (rules_file, ext);
    
-  
-  vector<string> vs = string_split (rules_file_data, "###");
-  
-  //vs[0] = vs[0].insert (3, "@testkey=testvalue\n"); 
- // vs[0] = vs[0].insert (3, ";a=a\u000A"); 
- //  vs[0] = vs[0].insert (3, ";080808808909\u000A\n"); 
- 
-  
-  
-  for (size_t i = 0; i < vs.size(); i++)
-     {
-      string fname = files[i];
-      
-      cout << "process:" << fname << endl;
-      
-      TagLib::FileRef f (fname.c_str());
-      
-      CPairFile pf (vs[i], true);
-
-  /*
-   Nasty hack - the first tag at the first tags block, by the mystical reason, cannot be
-   parsed properly.
-   There are two medicines from this STRANGE behaviour.
-   1. Manually (but not programmatically from mtag) 
-      add the obsolete first line to the RULES file.
-   2. Add the obsolete key-val to the tags structure.    
-   */
-
-      pf.values["@testkey"] = "testvalue";
-      
-      for (auto & kvp : pf.values)
-          {
-           string key = kvp.first;
-           string val = pf.values[kvp.first];
-           
-           cout << key << " --------------------- " << val << endl;
-
-           TagLib::String ts (val, TagLib::String::Type::UTF8);
-           
-           if (key == "@artist")
-               f.tag()->setArtist (ts);
-           else            
-           if (key == "@title")
-               f.tag()->setTitle (ts);
-           else            
-           if (key == "@album")
-               f.tag()->setAlbum (ts);
-           else            
-           if (key == "@genre")
-               f.tag()->setGenre (ts);
-           else            
-           if (key == "@comment")
-               f.tag()->setComment (ts);
-          /* else            
-           if (key == "@year")
-               f.tag()->setYear (atoi(val.c_str()));
-           else            
-           if (key == "@track")
-               f.tag()->setTrack (atoi(val.c_str()));*/
-           else            
-           if (key == "@year")
-               f.tag()->setYear (stoi (val));
-           else            
-           if (key == "@track")
-               f.tag()->setTrack (stoi (val));
-          
-          }
-
-         f.save();
-   
-         cout << "tag: saved" << endl;
-        }
+    }
+       
   
   
   return 0;
